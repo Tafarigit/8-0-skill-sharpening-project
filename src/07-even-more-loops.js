f0820bb21a6f12e41adbc9cf90fde1a7b55e4c29
Tafari Excell <excell.tafari@gmail.com>
2022-10-19 11:40:54 -0400

    8th answer set

--- src/07-even-more-loops.js
+++ src/07-even-more-loops.js
@@ -12,7 +12,11 @@
  *  shoutForLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForLoop(array) {}
+function shoutForLoop(array) {
+ 
+}
+    
+ 
 
 /**
  * shoutWhileLoop()
@@ -28,7 +32,9 @@ function shoutForLoop(array) {}
  *  shoutWhileLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutWhileLoop(array) {}
+function shoutWhileLoop(array) {
+
+}
 
 /**
  * shoutForOfLoop()
@@ -44,7 +50,9 @@ function shoutWhileLoop(array) {}
  *  shoutForOfLoop([ "A", "Very", "Happy", "Array" ]);
  *  //> [ "A!", "Very!", "Happy!", "Array!" ];
  */
-function shoutForOfLoop(array) {}
+function shoutForOfLoop(array) {
+
+}
 
 /**
  * sumArray()
@@ -57,7 +65,14 @@ function shoutForOfLoop(array) {}
  *  sumArray([ 10, 0, 10, 11 ]);
  *  //> 31
  */
-function sumArray(array) {}
+function sumArray(array) {
+  let sum = 0 
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i]
+
+  }
+  return sum
+}
 
 /**
  * oddArray()
@@ -70,7 +85,18 @@ function sumArray(array) {}
  *  oddArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 11, 15, 37 ]
  */
-function oddArray(array) {}
+function oddArray(array) {
+  let oddArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 1) {
+      oddArr.push(array[i]);
+    }
+
+    }
+      return oddArr
+  }
+     
+
 
 /**
  * evenArray()
@@ -83,7 +109,16 @@ function oddArray(array) {}
  *  evenArray([ 11, 15, 20, 22, 37 ]);
  *  //> [ 20, 22 ]
  */
-function evenArray(array) {}
+function evenArray(array) {
+  let evenArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      evenArr.push(array[i]);
+    }
+
+    }
+      return evenArr
+}
 
 /**
  * findSmallest()
@@ -96,7 +131,10 @@ function evenArray(array) {}
  *  findSmallest([ 0, 11, -2, 5 ]);
  *  //> -2
  */
-function findSmallest(array) {}
+function findSmallest(array) {
+  
+      
+}
 
 /**
  * findLargest()
